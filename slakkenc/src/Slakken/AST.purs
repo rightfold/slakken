72b106eae24684d0355c4e5d6e6cb558babe577e
rightfold <rightfold@gmail.com>
2017-05-26 00:19:43 +0200

    Add Quote to AST

--- slakkenc/src/Slakken/AST.purs
+++ slakkenc/src/Slakken/AST.purs
@@ -1,10 +1,13 @@
 module Slakken.AST
   ( Name(..)
 
+  , Value(..)
+
   , Expr(..)
   , free
   ) where
 
+import Data.ByteString (ByteString)
 import Data.Set (Set)
 import Data.Set as Set
 import Control.Comonad.Cofree (Cofree, (:<))
@@ -18,10 +21,18 @@ data Name =
 
 --------------------------------------------------------------------------------
 
+data Value a
+  = Array (Array a)
+  | Atom ByteString
+  | Float Number
+
+--------------------------------------------------------------------------------
+
 data Expr a
   = Var (Either Name String)
   | App Name (Array a)
   | Abs (Array String) a
+  | Quote (Mu Value)
 
 derive instance fe :: Functor Expr
 
@@ -31,3 +42,4 @@ free = cata $ flip (:<) <*> case _ of
   Var (Right n) -> Set.singleton n
   App _ as -> foldMap extract as
   Abs ns a -> foldr Set.delete (extract a) ns
+  Quote _ -> Set.empty
